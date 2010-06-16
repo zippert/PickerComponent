b9a6aac0b14cae6c297dc5c625c7bd505ef36112
Mattias Zippert <mattias@zippert.org>
2010-06-16 14:42:54 +0200

    Fixed missing resources at check-in and fixed bug allowing blank items to be added.

---
+++ WW/NewItemDialog.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WW
+{
+    partial class NewItemDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(15, 26);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(182, 20);
+            this.keyTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Value:";
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Location = new System.Drawing.Point(15, 70);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(182, 20);
+            this.valueTextBox.TabIndex = 4;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // NewItemDialog
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 130);
+            this.Controls.Add(this.valueTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "NewItemDialog";
+            this.Text = "NewItemDialog";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ WW/NewItemDialog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WW
+{
+    public partial class NewItemDialog : Form
+    {
+        private PickerCreator pickerInstance;
+
+        public NewItemDialog(PickerCreator pickerInstance)
+        {
+            this.pickerInstance = pickerInstance;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!keyTextBox.Text.Equals("") && !valueTextBox.Text.Equals(""))
+            {
+                Boolean retVal = pickerInstance.saveNewValue(keyTextBox.Text, valueTextBox.Text);
+                if (!retVal)
+                {
+                    errorProvider1.SetError(button1, "This value already exists");
+                }
+                else
+                {
+                    errorProvider1.SetError(button1, "");
+                }
+            }
+            else
+            {
+                errorProvider1.SetError(button1, "Both fields must contain a valid content");
+            }
+        }
+    }
+}
